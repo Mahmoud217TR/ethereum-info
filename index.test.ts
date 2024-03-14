49f3340d444502633f3830119bebd6eab84439ef
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2024-03-15 01:12:11 +0300

    get usdt balance

--- index.test.ts
+++ index.test.ts
@@ -1,10 +1,24 @@
-import { getLastBlockNumber } from './index';
+import { getLastBlockNumber, balance } from './index';
 
 jest.mock('web3', () => {
  return jest.fn().mockImplementation(() => {
     return {
       eth: {
         getBlockNumber: jest.fn().mockResolvedValue(1234567),
+        Contract: jest.fn().mockImplementation(() => {
+          return {
+            methods: {
+              balanceOf: jest.fn().mockImplementation(() => {
+                return {
+                 call: jest.fn().mockResolvedValue('1000000000000000000'),
+                };
+              }),
+            },
+          };
+        }),
+      },
+      utils: {
+        fromWei: jest.fn().mockReturnValue('1000'),
       },
     };
  });
@@ -14,5 +28,10 @@ describe('Ethereum Info', () => {
  test('getLastBlockNumber returns the last block number', async () => {
     const blockNumber = await getLastBlockNumber();
     expect(blockNumber).toBe(1234567);
+ });
+
+ test('balance returns the USDT balance of a given address', async () => {
+    const amount = await balance('0x53FcbB0DA498BC3A5aDBDDD1c51dEE6dB02c2AC9');
+    expect(amount).toBe('1000');
  });
 });
--- index.ts
+++ index.ts
@@ -8,8 +8,34 @@ export async function getLastBlockNumber(): Promise<bigint>
     return blockNumber;
 }
 
+export async function balance(address): Promise<string>
+{
+    const usdtContractAddress = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
+    const usdtContractABI = [
+        {
+            "constant": true,
+            "inputs": [{
+                "name": "_owner",
+                "type": "address"
+            }],
+            "name": "balanceOf",
+            "outputs": [{
+                "name": "balance",
+                "type": "uint256"
+            }],
+            "type": "function"
+        }
+    ];
+    const usdtContract = new web3.eth.Contract(usdtContractABI, usdtContractAddress);
+    const balance: number = await usdtContract.methods.balanceOf(address).call();
+    return web3.utils.fromWei(balance, 'ether');
+}
 
 (async () => {
     const lastBlockNumber = await getLastBlockNumber()
     console.log(`Last Block Number: ${lastBlockNumber}`);
+    
+    const address = '0x53FcbB0DA498BC3A5aDBDDD1c51dEE6dB02c2AC9';
+    const amount = await balance(address);
+    console.log(`balance: ${amount}`);
 })();
